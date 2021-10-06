7de4e4c77804afa87a3003039c66e137031bf3d4
Oskar <actionman123@interia.pl>
2021-10-06 21:39:24 +0200

    final commit add styles to about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,23 @@
 import React from "react";
 
+import "../styles/AboutUs.css";
+
+import image from "../images/image6.jpg";
+
 const AboutUs = () => {
   return (
     <>
-      <div>
-        <p>aboutus</p>
-      </div>
+      <section className="aboutus">
+        <div className="info">
+          <h2>About Us</h2>
+          <p>
+            We are the special international charity fundation. We want to help
+            every single kids around the whole world and that's our passion.
+          </p>
+          <button>Read More</button>
+          <img src={image} alt="happy" />
+        </div>
+      </section>
     </>
   );
 };
