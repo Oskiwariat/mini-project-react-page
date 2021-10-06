7323d5e0cbd45bc79d3d5b7901b22764f0276889
Oskar <actionman123@interia.pl>
2021-10-06 20:50:19 +0200

    create style to joinus page

--- src/pages/JoinUs.js
+++ src/pages/JoinUs.js
@@ -1,11 +1,47 @@
 import React from "react";
 
+import "../styles/JoinUs.css";
+
+import image1 from "../images/image1.jpg";
+import image3 from "../images/image3.jpg";
+import image4 from "../images/image4.jpg";
+
 const JoinUs = () => {
   return (
     <>
-      <div>
-        <p>JoinUs</p>
-      </div>
+      <h1 className="special">Do you want to join us?</h1>
+      <section className="joinus">
+        <div className="first">
+          <h1>Help</h1>
+          <img src={image1} alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
+            provident totam voluptas molestiae voluptates consectetur quam
+            voluptate! Neque necessitatibus voluptatum suscipit provident sunt
+            quibusdam, officiis minima natus laborum fuga quae?
+          </p>
+        </div>
+        <div className="second">
+          <h1>Study</h1>
+          <img src={image3} alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
+            provident totam voluptas molestiae voluptates consectetur quam
+            voluptate! Neque necessitatibus voluptatum suscipit provident sunt
+            quibusdam, officiis minima natus laborum fuga quae?
+          </p>
+        </div>
+        <div className="third">
+          <h1>Involvement</h1>
+          <img src={image4} alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
+            provident totam voluptas molestiae voluptates consectetur quam
+            voluptate! Neque necessitatibus voluptatum suscipit provident sunt
+            quibusdam, officiis minima natus laborum fuga quae?
+          </p>
+        </div>
+      </section>
     </>
   );
 };
