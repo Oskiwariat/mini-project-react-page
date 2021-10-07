52f4e2481ce67ca029307bdeed935b7e35909675
Oskar <actionman123@interia.pl>
2021-10-08 00:25:11 +0200

    little modify about us page design and change join us page design

--- src/pages/JoinUs.js
+++ src/pages/JoinUs.js
@@ -2,44 +2,42 @@ import React from "react";
 
 import "../styles/JoinUs.css";
 
-import image1 from "../images/image1.jpg";
-import image3 from "../images/image3.jpg";
-import image4 from "../images/image4.jpg";
+import image from "../images/happykids.jpg";
 
 const JoinUs = () => {
   return (
     <>
-      <h1 className="special">Do you want to join us?</h1>
       <section className="joinus">
-        <div className="first">
-          <h1>Help</h1>
-          <img src={image1} alt="" />
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
-            provident totam voluptas molestiae voluptates consectetur quam
-            voluptate! Neque necessitatibus voluptatum suscipit provident sunt
-            quibusdam, officiis minima natus laborum fuga quae?
-          </p>
-        </div>
-        <div className="second">
-          <h1>Study</h1>
-          <img src={image3} alt="" />
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
-            provident totam voluptas molestiae voluptates consectetur quam
-            voluptate! Neque necessitatibus voluptatum suscipit provident sunt
-            quibusdam, officiis minima natus laborum fuga quae?
-          </p>
-        </div>
-        <div className="third">
-          <h1>Involvement</h1>
-          <img src={image4} alt="" />
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
-            provident totam voluptas molestiae voluptates consectetur quam
-            voluptate! Neque necessitatibus voluptatum suscipit provident sunt
-            quibusdam, officiis minima natus laborum fuga quae?
-          </p>
+        <div className="middlecontent">
+          <nav>
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">About Us</a>
+              </li>
+              <li>
+                <a href="">Work</a>
+              </li>
+              <li>
+                <a href="">Info</a>
+              </li>
+              <li>
+                <a href="">Get Started</a>
+              </li>
+            </ul>
+          </nav>
+          <img src={image} alt="" />
+          <div className="infosquare">
+            <h1>Join Our Community</h1>
+            <p>
+              We are looking for very active and wonderful people who love to
+              bring happiness and help other people. If you are such a person,
+              go ahead and click the button below and join us!
+            </p>
+            <button>Join Now</button>
+          </div>
         </div>
       </section>
     </>
