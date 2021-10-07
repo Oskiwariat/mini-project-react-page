1a38ddbb112ec2bdc55af7a55de53e3073dc4ba0
Oskar <actionman123@interia.pl>
2021-10-08 00:53:36 +0200

    mainpage header design background style with linear-gradient

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,20 +9,22 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <>
-      <header className="welcome">
-        <Navigation />
-        <div className="fundation">
-          <h1>International Charity fundation</h1>
-          <p>We love help</p>
-        </div>
-        <img src={bgcPhoto} alt="backgroundPhoto" />
-        <div className="about">
-          <h2>
-            School in Zambia is almost finished. <strong>Hurra!</strong>
-          </h2>
-          <button>See all projects</button>
-        </div>
-      </header>
+      <section className="mainpage">
+        <header className="welcome">
+          <Navigation />
+          <div className="fundation">
+            <h1>International Charity fundation</h1>
+            <p>We love help</p>
+          </div>
+          <img src={bgcPhoto} alt="backgroundPhoto" />
+          <div className="about">
+            <h2>
+              School in Zambia is almost finished. <strong>Hurra!</strong>
+            </h2>
+            <button>See all projects</button>
+          </div>
+        </header>
+      </section>
     </>
   );
 };
